a24e794e34c7cd59bc99433eb4486d6bf7e2cbc4
Gregorio <gregoriomoraga@gmail.com>
2023-07-21 16:08:29 -0400

    commit inicial

---
+++ comentarios.py
@@ -0,0 +1,7 @@
+# aqui definimos las variables
+a = "hola"
+b = "adios"
+""" imprimimos en pantalla los valores 
+de las variables a y b """
+# print(a)
+print(b)
---
+++ condicionales.py
@@ -0,0 +1,12 @@
+# variables
+a = 2
+b = 2
+
+# ejemplo de sentencia "if"
+if a < b:
+    print(f"{a} es menor que {b}")
+else:
+    if a > b:
+        print(f"{a} es mayor que {b}")
+    else:
+        print(f"{a} es igual que {b}") 
